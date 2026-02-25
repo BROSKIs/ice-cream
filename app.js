d5e71f2e6e1c766dac1c018b18ae35f2caa14236
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-02-25 13:27:43 -0800

    base styles for for confirmation and spell checking

--- app.js
+++ app.js
@@ -7,37 +7,37 @@ const app = express();
 app.set("view engine", "EJS");
 
 //PORT number
-const PORT = 3000;
+const PORT = 4729;
 
-const orders = []
+const orders = [];
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //Defining the default route
 app.get("/", (req, res) => {
-    res.render("home");
-})
+  res.render("home");
+});
 
-app.post("/submit", (req, res)=>{
-    // name= email= flavor= cone= Toppings comments= 
-    const order = {
-        name: req.body.name,
-        email: req.body.email,
-        flavor: req.body.flavor,
-        cone: req.body.cone,
-        toppings: req.body.toppings,
-        comment: req.body.comment
-    }
-    orders.push(order);
-    res.render("confirmation", { order });
+app.post("/submit", (req, res) => {
+  // name= email= flavor= cone= Toppings comments=
+  const order = {
+    name: req.body.name,
+    email: req.body.email,
+    flavor: req.body.flavor,
+    cone: req.body.cone,
+    toppings: req.body.toppings,
+    comment: req.body.comment,
+  };
+
+  console.log(Array.isArray(order.toppings));
+  orders.push(order);
+  res.render("confirmation", { order });
 });
 
 // Start the server and listen on the specified port
 
 app.listen(PORT, () => {
-
-    console.log(`Server is running at http://localhost:${PORT}`);
-
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,39 @@
 document.getElementById("order").onsubmit = validate;
 
-function validate(){
-    clearErrors();
-    let isValid = true;
-    let name = document.getElementById("name").value.trim();
-    let email = document.getElementById("email").value.trim();
+function validate() {
+  clearErrors();
+  let isValid = true;
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
 
-    if(!name){
-        document.getElementById("name-error").style.display = "inline-block";
-        isValid = false;
-    }
-    if(!email || !email.includes("@")){
-        document.getElementById("email-error").style.display = "inline-block";
-        isValid=false;
-    }
+  if (!name) {
+    document.getElementById("name-error").style.display = "inline-block";
+    isValid = false;
+  }
+  if (!email || !email.includes("@")) {
+    document.getElementById("email-error").style.display = "inline-block";
+    isValid = false;
+  }
 
-    let cup = document.getElementById("Cup");
-    let cup1 = document.getElementById("Sugar-Cone");
-    let cup2 = document.getElementById("Waffel-Cone");
-    if(!cup.checked && !cup1.checked && !cup2.checked){
-        document.getElementById("cone-error").style.display = "inline-block";
-        isValid=false;
-    }
-    let flavor = document.getElementById("flavor").value;
-    if(flavor=="none"){
-        document.getElementById("flavor-error").style.display = "inline-block";
-        isValid=false;
-    }
+  let cup = document.getElementById("Cup");
+  let cup1 = document.getElementById("Sugar-Cone");
+  let cup2 = document.getElementById("Waffle-Cone");
+  if (!cup.checked && !cup1.checked && !cup2.checked) {
+    document.getElementById("cone-error").style.display = "inline-block";
+    isValid = false;
+  }
+  let flavor = document.getElementById("flavor").value;
+  if (flavor == "none") {
+    document.getElementById("flavor-error").style.display = "inline-block";
+    isValid = false;
+  }
 
-    return isValid;
+  return isValid;
 }
 
-function clearErrors(){
-    let errors = document.getElementsByClassName("err");
-    for(let i=0; i<errors.length; i++){
-        errors[i].style.display = "none";
-    }
+function clearErrors() {
+  let errors = document.getElementsByClassName("err");
+  for (let i = 0; i < errors.length; i++) {
+    errors[i].style.display = "none";
+  }
 }
